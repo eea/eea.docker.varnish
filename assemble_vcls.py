2a2ac3e6a2402abbddd4d0e8e2ac645dbdcae1f5
Razvan Chitu <razvan.chitu@eaudeweb.ro>
2015-07-16 18:57:21 +0300

    Refs #26648 - Added scripts required to start and reload the Varnish daemon

---
+++ assemble_vcls.py
@@ -0,0 +1,24 @@
+import subprocess
+
+def add_includes(g):
+    print >> g
+    p = subprocess.Popen(["ls", "/conf.d"], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    includes = out.split("\n")[0:-1]
+    includes.sort()
+    for include in includes:
+        if ".vcl" not in include:
+            continue
+        print >> g, 'include "/conf.d/' + include + '";'
+
+
+g = open("/etc/varnish/temp_default.vcl", "w")
+with open("/etc/varnish/default.vcl", "r") as f, open("/etc/varnish/temp_default.vcl", "w") as g:
+    for line in f:
+        if line[0] is not "#" and "include" in line:
+            continue
+        print >> g, line,
+
+    add_includes(g)
+
+subprocess.call(["mv", "/etc/varnish/temp_default.vcl", "/etc/varnish/default.vcl"])
---
+++ reload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Make a copy of the existing configuration file
+cp /etc/varnish/default.vcl /etc/varnish/old_default.vcl
+python /assemble_vcls.py
+
+# Generate a name for the new configuration
+NEW_CONFIG_NAME="boot-$RANDOM"
+
+# Attempt to load the new configuration
+varnishadm "vcl.load $NEW_CONFIG_NAME /etc/varnish/default.vcl"
+if [ $? -eq 0 ]; then
+    # If the load succeeds, remove existing configurations and use the new one
+    varnishadm "vcl.use $NEW_CONFIG_NAME"
+    for CONFIG_NAME in $(varnishadm vcl.list | grep available | awk -F " " '{print $NF}'); do
+        varnishadm "vcl.discard $CONFIG_NAME"
+    done
+    rm /etc/varnish/old_default.vcl
+    echo "==== SUCCESSFULLY RELOADED VCL FILE ===="
+else
+    # If the load fails, restore the old configuration file
+    mv /etc/varnish/old_default.vcl /etc/varnish/default.vcl
+    echo "======     FAILURE TO RELOAD      ======"
+    echo "====== PLEASE CHECK CONFIGURATION ======"
+fi
