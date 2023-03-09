cdb095660a3f8afb35da0878787648cb9e2c96c1
valentinab25 <valentinab25>
2023-03-09 11:08:54 +0200

    Fix timeout variable update

--- varnish/src/update_timetokeep.sh
+++ varnish/src/update_timetokeep.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 
-VARNISH_TTL=${VARNISH_TTL:-60s}
-VARNISH_GRACE=${VARNISH_GRACE:-120s}
-VARNISH_KEEP=${VARNISH_KEEP:-120s}
+# VARNISH_TTL=${VARNISH_TTL:-60s}
+# VARNISH_GRACE=${VARNISH_GRACE:-120s}
+# VARNISH_KEEP=${VARNISH_KEEP:-120s}
 
-if [ -f /etc/varnish/conf.d/varnish.vcl ]; then
+  if [ -n "$VARNISH_TTL" ]; then
+    find /etc/varnish -type f -name "*.vcl" -exec sed -i "s/set beresp.ttl = [1-9][0-9]*s/set beresp.ttl = $VARNISH_TTL/g" {} +
+  fi
 
-  sed -i "s/set beresp.ttl = [1-9][0-9]*s/set beresp.ttl = $VARNISH_TTL/g"   /etc/varnish/conf.d/varnish.vcl
+  if [ -n "$VARNISH_GRACE" ]; then
+    find /etc/varnish -type f -name "*.vcl" -exec sed -i "s/set beresp.grace = .*/set beresp.grace = $VARNISH_GRACE;/g" {} +
+  fi
 
-  sed -i "s/set beresp.grace = .*/set beresp.grace = $VARNISH_GRACE;/g" /etc/varnish/conf.d/varnish.vcl
+  if [ -n "$VARNISH_KEEP" ]; then
+    find /etc/varnish -type f -name "*.vcl" -exec sed -i "s/set beresp.keep = .*/set beresp.keep = $VARNISH_KEEP;/g" {} +
+  fi
 
-  sed -i "s/set beresp.keep = .*/set beresp.keep = $VARNISH_GRACE;/g"  /etc/varnish/conf.d/varnish.vcl
-
-fi
 
 
