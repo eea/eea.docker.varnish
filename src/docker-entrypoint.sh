44b7ef828c8535ef490ac5ba1f4dd75df2f24ee8
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2024-12-16 19:29:12 +0200

    add debug message

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -21,6 +21,7 @@ fi
 # In case you wanted to keep the template vcl files in a read-only volume, use /varnish-config to store them
 
 if [ -d /varnish-config ] && [  $(find /varnish-config -type f -name "*.vcl" | wc -l) -ne 0  ] ; then
+   echo "Found /varnish-config volume templates. Will now copy them to /etc/varnish"
    rm -rf /etc/varnish
    cp -pr /varnish-config /etc/varnish
    chown -R varnish:varnish /etc/varnish
